91cab11d1eea2cf985e86814a69c41da7f8bdeba
Seweryn Sitarski <seweryn.sitarski@p.lodz.pl>
2023-01-13 19:47:34 +0100

    Zmiana procedury obsługi wyjątków

--- etc/bocm/functions.sh
+++ etc/bocm/functions.sh
@@ -18,56 +18,38 @@ EOF
   exit
 fi
 
-set -E -o functrace
-## Outputs Front-Mater formatted failures for functions not returning 0
-## Use the following line after sourcing this file to set failure trap
-##    trap 'failure "LINENO" "BASH_LINENO" "${BASH_COMMAND}" "${?}"' ERR
-failure(){
-    local -n _lineno="${1:-LINENO}"
-    local -n _bash_lineno="${2:-BASH_LINENO}"
-    local _last_command="${3:-${BASH_COMMAND}}"
-    local _code="${4:-0}"
-
-    ## Workaround for read EOF combo tripping traps
-    if ! ((_code)); then
-        return "${_code}"
-    fi
-
-    local _last_command_height
-    _last_command_height="$(wc -l <<<"${_last_command}")"
+set -E
+function handle_exception() {
+    local _lineno="${1:-LINENO}"
+    local _bash_lineno="${2:-BASH_LINENO}"
+    local _last_code_line="${3}"
+    local _last_command="${4}"
+    local _code="${5}"
 
     local -a _output_array=()
+
     _output_array+=(
-        '---'
-        "lines_history: [${_lineno} ${_bash_lineno[*]}]"
-        "function_trace: [${FUNCNAME[*]}]"
-        "exit_code: ${_code}"
+        "*** Exception ***"
+        "  "
+        "   Source line: ${_last_code_line}"
+        "       Command: ${_last_command}"
+        "   Line number: $_lineno"
+        "Function_trace: [${FUNCNAME[*]:1}] ${BASH_LINENO[*]::$((${#FUNCNAME[@]}-1))}"
+        "     Exit code: ${_code}"
+        "  "
+        "***************"
     )
+    # echo "  Function call stack:"
+    # for i in "${func_stack[@]}"; do
+    #     echo "  $i"
+    # done
 
-    if [[ "${#BASH_SOURCE[@]}" -gt '1' ]]; then
-        _output_array+=('source_trace:')
-        for _item in "${BASH_SOURCE[@]}"; do
-            _output_array+=("  - ${_item}")
-        done
-    else
-        _output_array+=("source_trace: [${BASH_SOURCE[*]}]")
-    fi
-
-    if [[ "${_last_command_height}" -gt '1' ]]; then
-        _output_array+=(
-            'last_command: ->'
-            "${_last_command}"
-        )
-    else
-        _output_array+=("last_command: ${_last_command}")
-    fi
-
-    _output_array+=('---')
     printf '%s\n' "${_output_array[@]}" >&2
-    return ${_code}
+
+    [[ $(type -t panic) == function ]] && panic || exit ${_code}
 }
 
-trap 'failure "LINENO" "BASH_LINENO" "${BASH_COMMAND}" "${?}"' ERR
+trap 'handle_exception "${LINENO}" "${BASH_LINENO}" "${BASH_COMMAND}" "$(eval echo ${BASH_COMMAND})" "${?}"' ERR
 
 
 # Load default, then allow override.
@@ -805,7 +787,7 @@ bocm_bottom() {
     #     && exit"
 
     if [ -z ${PRE_BOOT_FILE} ]; then PRE_BOOT_FILE=/etc/pre_boot; fi
-    
+
     if [ -x ${rootmnt}${PRE_BOOT_FILE} ]; then
       chroot ${rootmnt} /bin/bash -c "${PRE_BOOT_FILE}; exit"
     else
---
+++ tests/test_exep.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#set -E -o functrace
+set -E
+# declare -a func_stack
+
+function handle_exception() {
+    local _lineno="${1:-LINENO}"
+    local _bash_lineno="${2:-BASH_LINENO}"
+    local _last_code_line="${3}"
+    local _last_command="${4}"
+    local _code="${5}"
+
+    local -a _output_array=()
+
+    _output_array+=(
+        "*** Exception ***"
+        "  "
+        "   Source line: ${_last_code_line}"
+        "       Command: ${_last_command}"
+        "   Line number: $_lineno"
+        "Function_trace: [${FUNCNAME[*]:1}] ${BASH_LINENO[*]::$((${#FUNCNAME[@]}-1))}"
+        "     Exit code: ${_code}"
+        "  "
+        "***************"
+    )
+    # echo "  Function call stack:"
+    # for i in "${func_stack[@]}"; do
+    #     echo "  $i"
+    # done
+
+    printf '%s\n' "${_output_array[@]}" >&2
+
+    [[ $(type -t panic) == function ]] && panic || exit ${_code}
+}
+
+trap 'handle_exception "${LINENO}" "${BASH_LINENO}" "${BASH_COMMAND}" "$(eval echo ${BASH_COMMAND})" "${?}"' ERR
+
+# function push_func() {
+#     local func_info="${FUNCNAME[1]} line number ${BASH_LINENO[0]}"
+#     func_stack+=("${func_info}")
+# }
+
+# function pop_func() {
+#     unset 'func_stack[-1]'
+# }
+
+ZMIENNA="tresc_zmienna"
+
+_funcPierwsza() {
+    # push_func
+    local ZMIENNA1="tresc_zmienna_1"
+
+    # echo ${1} ${2} ${ZMIENNA1}
+    ls /sew ${1} ${2} ${ZMIANNA1}
+    # pop_func
+}
+
+_funcDruga() {
+    # push_func
+    local ZMIENNA2="tresc_zmienna_2"
+
+    
+    _funcPierwsza ${1} ${ZMIENNA2}
+    # pop_func
+}
+
+_funcDruga ${ZMIENNA}
